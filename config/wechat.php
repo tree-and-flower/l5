79edc4f2242338b0570911c75889ff17433c0bd3
zhangjingwei <306824269@qq.com>
2015-04-11 23:08:53 +0800

    wechat.php.example

--- config/wechat.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'appId'          => 'wx8aaccc0fdf243bc2', // 必填
-    'secret'         => '46cca253e767b4911517cda92ab53f92', // 必填
-    'token'          => 'zhangjingwei',  // 必填
-    'encodingAESKey' => 'Your encoding AES Key' // 加密模式需要，其它模式不需要
-];
