c02468db7cb3e5e0f43b7898e7440b1e3faae290
zhangjingwei <306824269@qq.com>
2015-04-16 18:36:23 +0800

    wechat more further

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -11,12 +11,17 @@ END;
 请输入景点名，即可查询该景点门票价格、开放时间等详细信息。如海洋世界、世界之窗、欢乐谷、锦绣中华民俗村等。
 END;
     const TEXT_TICKET_BOOK = <<<END
-输入景点名（如世界之窗）查询门票价格、开放时间等详细信息。门票预定接受两种预定方式，预定时间截止到出行时间的前天晚上12点。
-预定方式1：
-直接回复预定信息：[姓名][联系方式][景点名称][预定人数]
-例：张三，18828282828，世界之窗，3人
-预定方式2：
-通过以下联系进行预定：
+输入景点名（如世界之窗）查询门票价格、开放时间等详细信息。门票预订接受两种预订方式，预订时间截止到出行时间的前天晚上12点。
+预订方式1：直接回复信息，并且以“预订”或者“预定”开始
+格式：[预订：][姓名][联系方式][景点名称][预订人数]
+例：预订：张三，18828282828，世界之窗，3人
+预订方式2：
+通过以下联系进行预订：
+QQ:306824269
+TEL:13422872077
+END;
+    const TEXT_TICKET_BOOK_SUCCESS = <<<END
+感谢您的预订，我们已经成功收到您的预订信息，我们的工作人员会统一确认和处理您的预订信息，无效信息会直接过滤掉，请留意我们的确认信息，如需帮助请直接联系：
 QQ:306824269
 TEL:13422872077
 END;
@@ -49,9 +54,13 @@ END;
         });
         //普通消息只有文本类型时进行模糊匹配
         $this->wc->message(function($msg) use ($keyword) {
+            $msgContent = trim($msg->Content);
             if ('text' == $msg->MsgType) {
+                if (0 === strpos($msgContent, '预订')) {
+                    return Message::make('text')->content(self::TEXT_TICKET_BOOK_SUCCESS);
+                }
                 foreach ($keyword as $word => $content) {
-                    if (false !== strpos($msg->Content, $word)) {
+                    if (false !== strpos($msgContent, $word)) {
                         return Message::make('text')->content($content);
                     }
                 }
